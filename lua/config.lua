cd7ee656ea08161032ada177be55e46db53c9d76
PigeonCoding <wassimdzd@gmail.com>
2023-09-05 13:54:40 +0100

    configuration babyyyyy

--- lua/config.lua
+++ lua/config.lua
@@ -2,5 +2,11 @@ local opt = vim.opt
 
 opt.cursorline = true
 opt.number = true
-opt.relativenumber = true
+-- opt.relativenumber = true
 opt.termguicolors = true
+
+local TAB_WIDTH = 2
+opt.tabstop = TAB_WIDTH
+opt.softtabstop = TAB_WIDTH
+opt.shiftwidth = TAB_WIDTH
+
